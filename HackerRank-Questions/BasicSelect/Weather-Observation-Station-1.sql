c089f0ac998890456696e780c0ea0bfca9e5edb8
Annu Kumari <anuk35168@gmail.com>
2026-01-02 23:58:11 +0530

    small changes

--- HackerRank-Questions/BasicSelect/Weather-Observation-Station-1.sql
+++ HackerRank-Questions/BasicSelect/Weather-Observation-Station-1.sql
@@ -14,3 +14,23 @@ SELECT CITY, STATE
 FROM STATION;
 
 -- This query retrieves the CITY and STATE columns from the STATION table.
+--How to think about this problem (important)
+--Plain English:
+--1.I want city names and state codes
+--SQL mapping:
+--City names → SELECT CITY
+--State codes → SELECT STATE
+--Interview rule to remember forever
+--Selecting multiple columns → Separate column names with a comma
+--These appear constantly in SQL interviews.
+--Key takeaway (very important)
+--Same logic, different SQL dialects.
+--Database	             Multiple column selection
+--MySQL / PostgreSQL	   SELECT col1, col2
+--DB2	                   SELECT col1, col2
+--Oracle	               SELECT col1, col2
+--Interview-level insight
+--If interviewer asks:
+--“How do you select multiple columns?”
+--Best answer:
+--“Separate the column names with a comma in the SELECT statement.”
